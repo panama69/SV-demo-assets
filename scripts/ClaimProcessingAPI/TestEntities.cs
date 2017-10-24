bab82a5d83077b612e0f0b39e1662ded777a971b
Dave Flynn <david.flynn@hpe.com>
2017-10-24 08:32:14 -0500

    init

---
+++ scripts/ClaimProcessingAPI/TestEntities.cs
@@ -0,0 +1,35 @@
+using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using HP.ST.Fwk.RunTimeFWK.Utilities;
+    using HP.ST.Fwk.RunTimeFWK.BindingFWK;
+    
+    namespace Script
+    {
+    
+    public class TestEntities
+    {
+    public ISTRunTimeContext Context = null;
+    public Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource> dataSourceNameToDataSource = new Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource>();
+    
+    protected HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource GetDataSource(string dataSourceName)
+    {
+    if(!dataSourceNameToDataSource.ContainsKey(dataSourceName))
+    	throw new Exception(("A data source with the specified name does not exist."));
+    return dataSourceNameToDataSource[dataSourceName];
+    }
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity14 = null;
+    public HP.ST.Ext.BasicActivities.StartActivity StartActivity1 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop2Input> Loop2 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity16 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterCloseActivity DataExporterCloseActivity17 = null;
+    public HP.ST.Ext.BasicActivities.EndActivity EndActivity3 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity15 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence13 = null;
+    public HP.ST.Ext.WebServicesActivities.StServiceCallActivity StServiceCallActivity4 = null;
+    public HP.ST.Ext.WebServicesActivities.StServiceCallActivity StServiceCallActivity5 = null;
+    
+    }
+    
+    }
+    
